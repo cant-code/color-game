b5bdd83d71747d6ebb05758ca4a9a1b02fe8b2fa
Ayush <ayushc13000@gmail.com>
2020-01-27 23:38:28 +0530

    Fixed Jumbotron text size and added losing capability and hard difficulty.

--- color.js
+++ color.js
@@ -2,23 +2,31 @@ var difficulty = 'hard';
 var r = 0;
 var g = 0;
 var b = 0;
+var chances = 1;
 
 document.getElementById("easy").onclick = function () {
-    document.getElementById("hard").classList.remove("btn-primary");
-    document.getElementById("easy").classList.remove("btn-outline-primary");
-    document.getElementById("easy").classList.add("btn-primary");
     difficulty = 'easy';
     document.getElementById("hardef").classList.remove("show");
+    document.getElementById("dif_med").classList.remove("show");
     document.getElementById("hardef").classList.add("hide");
+    document.getElementById("dif_med").classList.add("hide");
     gen();
 };
 
 document.getElementById("hard").onclick = function () {
-    document.getElementById("easy").classList.remove("btn-primary");
-    document.getElementById("hard").classList.remove("btn-outline-primary");
-    document.getElementById("hard").classList.add("btn-primary");
     difficulty = 'hard';
     document.getElementById("hardef").classList.remove("hide");
+    document.getElementById("dif_med").classList.remove("show");
+    document.getElementById("hardef").classList.add("show");
+    document.getElementById("dif_med").classList.add("hide");
+    gen();
+};
+
+document.getElementById("medium").onclick = function () {
+    difficulty = 'medium';
+    document.getElementById("hardef").classList.remove("hide");
+    document.getElementById("dif_med").classList.remove("hide");
+    document.getElementById("hardef").classList.add("show");
     document.getElementById("hardef").classList.add("show");
     gen();
 };
@@ -33,7 +41,7 @@ function gen() {
     g = Math.floor(Math.random() * 256);
     b = Math.floor(Math.random() * 256);
     document.getElementById("display-2").textContent = "RGB(" + r + ", " + g + ", " + b + ")";
-    if (difficulty === 'hard') {
+    if (difficulty === 'medium') {
         var boxes = document.getElementsByClassName("square");
         var randomBox = Math.floor(Math.random() * boxes.length);
         for (var i = 0; i < boxes.length; i++) {
@@ -42,7 +50,17 @@ function gen() {
                 + ')';
         }
         boxes[randomBox].style.background = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-    } else {
+    } else if (difficulty === 'hard') {
+        var boxes = document.getElementsByClassName("medium");
+        var randomBox = Math.floor(Math.random() * boxes.length);
+        for (var i = 0; i < boxes.length; i++) {
+            boxes[i].classList.remove("hide");
+            boxes[i].style.background = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256)
+                + ')';
+        }
+        boxes[randomBox].style.background = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    }
+    else {
         var boxes = document.getElementsByClassName("easy");
         var randomBox = Math.floor(Math.random() * boxes.length);
         for (var i = 0; i < boxes.length; i++) {
@@ -52,32 +70,38 @@ function gen() {
         }
         boxes[randomBox].style.background = 'rgb(' + r + ', ' + g + ', ' + b + ')';
     }
-};
+    document.getElementById("dif_med").focus();
+}
 
 var things = document.getElementsByClassName("square");
 
 for (var i = 0; i < things.length; i++) {
     things[i].addEventListener("click", function () {
-        if (this.style.background === "rgb(" + r + ", " + g + ", " + b + ")") {
+        if (this.style.background.indexOf("rgb(" + r + ", " + g + ", " + b + ")") >=0 )  {
+            chances = 1;
             document.getElementById("success").innerHTML = "Correct!!";
-            win(i);
+            win();
         } else {
             document.getElementById("success").innerHTML = "Try Again!!!";
             this.classList.add("hide");
+            chances++;
+            if (((chances === 3) && (difficulty === 'easy')) || ((chances === 5) && (difficulty === 'hard')) || ((chances === 3) && (difficulty === 'medium'))) {
+                document.getElementById("success").innerHTML = "You Lost!!";
+                chances = 1;
+                win();
+            }
         }
     });
 }
 
-function win(no) {
+function win() {
     document.getElementById("jumbotron").style.background = 'rgb(' + r + ', ' + g + ', ' + b + ')';
     document.getElementById("jumbotron").classList.remove("bg-primary");
     var boxes = document.getElementsByClassName("square");
     for (var i = 0; i < boxes.length; i++) {
-        if (i !== no) {
             boxes[i].style.background = 'rgb(' + r + ', ' + g + ', ' + b + ')';
             boxes[i].classList.remove("hide");
             boxes[i].classList.add("show");
-        }
     }
     document.getElementById("new_color").innerText = "Play Again?";
 }
